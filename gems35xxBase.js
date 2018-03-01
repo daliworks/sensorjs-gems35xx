bcb213e007cdd2da4b2cb6bb0defe031f837fec1
Inhyun Cho <inhyun.cho@daliworks.net>
2018-03-01 13:12:21 +0900

    Change to default configurations.

--- gems35xxBase.js
+++ gems35xxBase.js
@@ -33,26 +33,26 @@ function Gems35xxBase (parent) {
   self.interval = 10000;
   self.registerAddress = 30000;
   self.registerCount = 104;  
-  self.items = [
-    { field: 'temperature',     value: 0, registered: false, offset: 1,  type: 'readUInt16BE', converter: TemperatureConverter },
-    { field: 'frequency',       value: 0, registered: false, offset: 2,  type: 'readUInt16BE', converter: FrequencyConverter},
-    { field: 'V123LNAverage',   value: 0, registered: false, offset: 64, type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'V123LLAverage',   value: 0, registered: false, offset: 66, type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'V123LNUnbalance', value: 0, registered: false, offset: 68, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'V123LLUnbalance', value: 0, registered: false, offset: 69, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'V1',              value: 0, registered: false, offset: 70, type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'V12',             value: 0, registered: false, offset: 72, type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'V1Unbalance',     value: 0, registered: false, offset: 74, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'V12Unbalance',    value: 0, registered: false, offset: 75, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'V2',              value: 0, registered: false, offset: 76, type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'V23',             value: 0, registered: false, offset: 78, type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'V2Unbalance',     value: 0, registered: false, offset: 80, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'V23Unbalance',    value: 0, registered: false, offset: 81, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'V3',              value: 0, registered: false, offset: 82, type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'V31',             value: 0, registered: false, offset: 84, type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'V3Unbalance',     value: 0, registered: false, offset: 86, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'V31Unbalance',    value: 0, registered: false, offset: 87, type: 'readUInt16BE', converter: ValueConverter}  
-  ];
+  self.items = {
+    temperature:    { value: 0, registered: false, offset: 1,  type: 'readUInt16BE', converter: TemperatureConverter },
+    frequency:      { value: 0, registered: false, offset: 2,  type: 'readUInt16BE', converter: FrequencyConverter},
+    V123LNAverage:  { value: 0, registered: false, offset: 64, type: 'readUInt32BE', converter: ValueConverter},
+    V123LLAverage:  { value: 0, registered: false, offset: 66, type: 'readUInt32BE', converter: ValueConverter},
+    V123LNUnbalance:{ value: 0, registered: false, offset: 68, type: 'readUInt16BE', converter: ValueConverter},
+    V123LLUnbalance:{ value: 0, registered: false, offset: 69, type: 'readUInt16BE', converter: ValueConverter},
+    V1:             { value: 0, registered: false, offset: 70, type: 'readUInt32BE', converter: ValueConverter},
+    V12:            { value: 0, registered: false, offset: 72, type: 'readUInt32BE', converter: ValueConverter},
+    V1Unbalance:    { value: 0, registered: false, offset: 74, type: 'readUInt16BE', converter: ValueConverter},
+    V12Unbalance:   { value: 0, registered: false, offset: 75, type: 'readUInt16BE', converter: ValueConverter},
+    V2:             { value: 0, registered: false, offset: 76, type: 'readUInt32BE', converter: ValueConverter},
+    V23:            { value: 0, registered: false, offset: 78, type: 'readUInt32BE', converter: ValueConverter},
+    V2Unbalance:    { value: 0, registered: false, offset: 80, type: 'readUInt16BE', converter: ValueConverter},
+    V23Unbalance:   { value: 0, registered: false, offset: 81, type: 'readUInt16BE', converter: ValueConverter},
+    V3:             { value: 0, registered: false, offset: 82, type: 'readUInt32BE', converter: ValueConverter},
+    V31:            { value: 0, registered: false, offset: 84, type: 'readUInt32BE', converter: ValueConverter},
+    V3Unbalance:    { value: 0, registered: false, offset: 86, type: 'readUInt16BE', converter: ValueConverter},
+    V31Unbalance:   { value: 0, registered: false, offset: 87, type: 'readUInt16BE', converter: ValueConverter}  
+  };
   self.on('done', function (registers) {
     self.items.forEach(function (item) {
       var buffer = new Buffer(4);
@@ -86,27 +86,24 @@ function Gems35xxBaseCreate(address, port) {
 
 Gems35xxBase.prototype.registerField = function(sensor) {
   var self = this;
-  var i;
 
-  for(i = 0 ; i < self.items.length ; i++) {
-    if (self.items[i].field == sensor.field) {
-      self.items[i].registered = true;
-      self.parent.run();
-      return;
-    }
+  if (self.items[sensor.field] != undefined) {
+    self.items[sensor.field].registered = true;
+    self.parent.run();
+  }
+  else{
+    logger.error('Undefined field tried to register : ', sensor.field);
   }
 }
 
 Gems35xxBase.prototype.getValue = function (sensor) {
   var self = this;
 
-  var i;
-  for(i = 0 ; i < self.items.length ; i++) {
-    if (self.items[i].field == sensor.field) {
-      return  self.items[i].value;
-    }
+  if (self.items[sensor.field] != undefined) {
+    return  self.items[sensor.field].value;
   }
 
+  logger.error('Tried to get value of undefined field : ', sensor.field);
   return  undefined;
 }
 
--- gems35xxFeeder.js
+++ gems35xxFeeder.js
@@ -41,32 +41,33 @@ function Gems35xxFeeder (parent, id) {
   self.interval = 10000;
   self.registerAddress = 30080 + id * 40;
   self.registerCount = 40;
-  self.items = [
-    { field: 'type',            value: 0, registered: false, offset: 0,  type: 'readUInt16BE', converter: FeederType },
-    { field: 'leakageCurrent',  value: 0, registered: false, offset: 1,  type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'current',         value: 0, registered: false, offset: 2,  type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'power',           value: 0, registered: false, offset: 4,  type: 'readInt32BE',  converter: ValueConverter},
-    { field: 'VAR',             value: 0, registered: false, offset: 6,  type: 'readInt32BE',  converter: undefined},
-    { field: 'VA',              value: 0, registered: false, offset: 8,  type: 'readUInt32BE', converter: undefined},
-    { field: 'PFAverage',       value: 0, registered: false, offset: 10, type: 'readInt16BE',  converter: ValueConverter},
-    { field: 'currentUnbalance',value: 0, registered: false, offset: 12, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'THDAverage',      value: 0, registered: false, offset: 13, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'L1Current',       value: 0, registered: false, offset: 16, type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'L1Power',         value: 0, registered: false, offset: 18, type: 'readInt32BE',  converter: ValueConverter},
-    { field: 'L1Phase',         value: 0, registered: false, offset: 20, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'L1PowerFactor',   value: 0, registered: false, offset: 21, type: 'readInt16BE',  converter: ValueConverter},
-    { field: 'L1PowerTHD',      value: 0, registered: false, offset: 22, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'L2Current',       value: 0, registered: false, offset: 24, type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'L2Power',         value: 0, registered: false, offset: 26, type: 'readInt32BE',  converter: ValueConverter},
-    { field: 'L2Phase',         value: 0, registered: false, offset: 28, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'L2PowerFactor',   value: 0, registered: false, offset: 29, type: 'readInt16BE',  converter: ValueConverter},
-    { field: 'L2PowerTHD',      value: 0, registered: false, offset: 30, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'L3Current',       value: 0, registered: false, offset: 32, type: 'readUInt32BE', converter: ValueConverter},
-    { field: 'L3Power',         value: 0, registered: false, offset: 34, type: 'readInt32BE',  converter: ValueConverter},
-    { field: 'L3Phase',         value: 0, registered: false, offset: 36, type: 'readUInt16BE', converter: ValueConverter},
-    { field: 'L3PowerFactor',   value: 0, registered: false, offset: 37, type: 'readInt16BE',  converter: ValueConverter},
-    { field: 'L3PowerTHD',      value: 0, registered: false, offset: 38, type: 'readUInt16BE', converter: ValueConverter}
-  ];
+  self.items = {
+    type:           { value: 0, registered: false, offset: 0,  type: 'readUInt16BE', converter: FeederType },
+    leakageCurrent: { value: 0, registered: false, offset: 1,  type: 'readUInt16BE', converter: ValueConverter},
+    current:        { value: 0, registered: false, offset: 2,  type: 'readUInt32BE', converter: ValueConverter},
+    power:          { value: 0, registered: false, offset: 4,  type: 'readInt32BE',  converter: ValueConverter},
+    VAR:            { value: 0, registered: false, offset: 6,  type: 'readInt32BE',  converter: undefined},
+    VA:             { value: 0, registered: false, offset: 8,  type: 'readUInt32BE', converter: undefined},
+    PFAverage:      { value: 0, registered: false, offset: 10, type: 'readInt16BE',  converter: ValueConverter},
+    currentUnbalance:{value: 0, registered: false, offset: 12, type: 'readUInt16BE', converter: ValueConverter},
+    THDAverage:     { value: 0, registered: false, offset: 13, type: 'readUInt16BE', converter: ValueConverter},
+    L1Current:      { value: 0, registered: false, offset: 16, type: 'readUInt32BE', converter: ValueConverter},
+    L1Power:        { value: 0, registered: false, offset: 18, type: 'readInt32BE',  converter: ValueConverter},
+    L1Phase:        { value: 0, registered: false, offset: 20, type: 'readUInt16BE', converter: ValueConverter},
+    L1PowerFactor:  { value: 0, registered: false, offset: 21, type: 'readInt16BE',  converter: ValueConverter},
+    L1PowerTHD:     { value: 0, registered: false, offset: 22, type: 'readUInt16BE', converter: ValueConverter},
+    L2Current:      { value: 0, registered: false, offset: 24, type: 'readUInt32BE', converter: ValueConverter},
+    L2Power:        { value: 0, registered: false, offset: 26, type: 'readInt32BE',  converter: ValueConverter},
+    L2Phase:        { value: 0, registered: false, offset: 28, type: 'readUInt16BE', converter: ValueConverter},
+    L2PowerFactor:  { value: 0, registered: false, offset: 29, type: 'readInt16BE',  converter: ValueConverter},
+    L2PowerTHD:     { value: 0, registered: false, offset: 30, type: 'readUInt16BE', converter: ValueConverter},
+    L3Current:      { value: 0, registered: false, offset: 32, type: 'readUInt32BE', converter: ValueConverter},
+    L3Power:        { value: 0, registered: false, offset: 34, type: 'readInt32BE',  converter: ValueConverter},
+    L3Phase:        { value: 0, registered: false, offset: 36, type: 'readUInt16BE', converter: ValueConverter},
+    L3PowerFactor:  { value: 0, registered: false, offset: 37, type: 'readInt16BE',  converter: ValueConverter},
+    L3PowerTHD:     { value: 0, registered: false, offset: 38, type: 'readUInt16BE', converter: ValueConverter}
+  };
+
   self.on('done', function (registers) {
     self.items.forEach(function (item) {
       var buffer = new Buffer(4);
@@ -102,25 +103,23 @@ Gems35xxFeeder.prototype.registerField = function(sensor) {
   var self = this;
   var i;
 
-  for(i = 0 ; i < self.items.length ; i++) {
-    if (self.items[i].field == sensor.field) {
-      self.items[i].registered = true;
-      self.parent.run();
-      return;
-    }
+  if (self.items[sensor.field] != undefined) {
+    self.items[sensor.field].registered = true;
+    self.parent.run();
+  }
+  else{
+    logger.error('Undefined field tried to register : ', sensor.field);
   }
 }
 
 Gems35xxFeeder.prototype.getValue = function (sensor) {
   var self = this;
 
-  var i;
-  for(i = 0 ; i < self.items.length ; i++) {
-    if (self.items[i].field == sensor.field) {
-      return  self.items[i].value;
-    }
+  if (self.items[sensor.field] != undefined) {
+    return  self.items[sensor.field].value;
   }
 
+  logger.error('Tried to get value of undefined field : ', sensor.field);
   return  undefined;
 }
 
